2ec5924fb49432d2e59147d17e5718b38c76bfec
Hasan Aga <careerforma@gmail.com>
2022-07-01 23:37:12 +0300

    get correct image name

--- _posts/correct-image-links.py
+++ _posts/correct-image-links.py
@@ -3,20 +3,46 @@ from os import listdir
 import sys
 import re
 
+#run with command line arg of filename (including extention) ex python3 edit_photo_link.py filename.md
+
+def handleCommandLineArgs():
+    if(len(sys.argv)<=1):
+        print("no command line arguments were found!")
+        return False
+    else:
+        filename = sys.argv[1]
+        print('Got filename from command line argument: ', filename)
+        return filename
+
+def isMd(file:str)->bool:
+    try:
+        result = file.index(".md") or file.index(".markdown")
+        print(f"{file} is a markdown file, processing...")
+        return True
+    except ValueError:
+        print(f"{file} is not markdown file!")
+        return False
+
+def removeFileNameExtension(filename:str)->str:
+    pattern = '\.(md|markdown)'
+    result = re.search(pattern, filename)
+    return filename[0:result.start()]
+
 
 def indexImageName(link:str):
     # find image name in url
-    pattern='[\w-]+\.(jpg|png|txt)'
-    return re.search(pattern, link).group()
+    imageNameIndex = link.rfind('/')
+    return link[imageNameIndex::]
 
 def isImageLink(line:str):
-    # regex detects ![untitled]
-    pattern = '!\[[A-Z*a-z*]*\]'
+    # regex detects ![any text]
+    pattern = '!\[(.*?)\]'
     result = re.search(pattern, line)
     return result
 
 def correctLink(filename, imageName):
-    correctImageLink = f"![](/assets/{filename}/{imageName}"
+    filename = removeFileNameExtension(filename)
+    correctImageLink = f"![](/assets/{filename}{imageName})"
     return correctImageLink
 
 def processFile(filename:str):
@@ -26,11 +52,17 @@ def processFile(filename:str):
     for lineNumber,line in enumerate(lines):
         if(isImageLink(line)):
             print("old: " + line)
-            print("new: " + correctLink(filename))
-            lines[lineNumber] = correctLink(filename, indexImageName(line))
+            newLink = correctLink(filename, indexImageName(line))
+            print("new: " + newLink)
+            lines[lineNumber] = newLink
     
     with open(filename, 'w') as outputFile:
         outputFile.writelines(lines)
+
+filename = handleCommandLineArgs()
+if(filename != False and isMd(filename)):
+    print("processing...")
+    processFile(filename)
 
 
 # tests
@@ -44,3 +76,6 @@ def processFile(filename:str):
 
 # test correctLink: must create correct image url
 # print(correctLink("2022-blog-post.md", indexImageName("![Untitled](Html-CSS%209360a589e4364ee18512ee052ecc52b3/Untitl_ed.png)")))
+
+# #test removeFileNameExtension(filename): must remove extension
+# print(removeFileNameExtension("2022-02-post.md"))
