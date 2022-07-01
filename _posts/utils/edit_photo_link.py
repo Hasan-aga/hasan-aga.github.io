3a4cedbe72c2516ca09fa64d5564a613a09b74ab
Hasan Aga <careerforma@gmail.com>
2022-07-01 20:10:19 +0300

    handle cmd args

--- _posts/utils/edit_photo_link.py
+++ _posts/utils/edit_photo_link.py
@@ -1,17 +1,31 @@
 from os import listdir
+import sys
 # get list of all files in dir -> filter for .md files
-# read file -> find line starting with ![] -> replace path with /assets/
+# read file -> find line starting with ![] -> replace path with /assets/<filename>/<imagename>.img
+
+def handleCommandLineArgs():
+    # total arguments
+    print(len(sys.argv))
+    if(len(sys.argv)<=1):
+        print("no command line arguments were found!")
+        return False
+    else:
+        filename = sys.argv[1]
+        print('Got filename from command line argument: ', filename)
+        return filename
+
+
 
 def isImageLink(line:str)-> bool:
     return line[:3] == "![]"
 
-def correctLink(line:str)->str:
+def correctLink(filename:str, line:str)->str:
     try:
         indexOfImageName = line.index("img/") + 4
     except ValueError as ve:
         print("could not find 'img/', returning same link")
         return line
-    correctImageLink = f"![](/assets/{line[indexOfImageName::]}"
+    correctImageLink = f"![](/assets/{filename}/{line[indexOfImageName::]}"
     return correctImageLink
 
 def processFile(filename:str):
@@ -21,8 +35,8 @@ def processFile(filename:str):
     for lineNumber,line in enumerate(lines):
         if(isImageLink(line)):
             print("old: " + line)
-            print("new: " + correctLink(line))
-            lines[lineNumber] = correctLink(line)
+            print("new: " + correctLink(filename, line))
+            lines[lineNumber] = correctLink(filename, line)
     
     with open(filename, 'w') as outputFile:
         outputFile.writelines(lines)
@@ -36,10 +50,13 @@ def isMd(file:str)->bool:
         print(f"{file} is not markdown file!")
         return False
 
-filesInDir = listdir()
-markdownFiles = []
-for file in filesInDir:
-    if(isMd(file)):
-        markdownFiles.append(file)
-for file in markdownFiles:
-    processFile(file)
+
+handleCommandLineArgs()
+# activate the following lines for batch processing all .md files in current directory
+# filesInDir = listdir()
+# markdownFiles = []
+# for file in filesInDir:
+#     if(isMd(file)):
+#         markdownFiles.append(file)
+# for file in markdownFiles:
+#     processFile(file)
